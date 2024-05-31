47212a0b0ca8003877ed7d4221458e0b38c4b8c6
AndrejRot1 <andrej.rot1@gmail.com>
2024-05-31 17:00:29 +0200

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -7,7 +7,7 @@ import { asset } from "$fresh/runtime.ts";
 export default function Page() {
   return (
     <p>
-      <a href={asset("test_translation.html")}>View brochure</a>
+      <a href={asset("./test_translation.html")}>View brochure</a>
     </p>
   );
 }
